067d917f656e8d1df1b52fd6e933286e01fca9f5
Raj Patel <riskyrick20@gmail.com>
2023-09-19 16:23:17 +0530

    added support for rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,21 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+const postImportResults = import.meta.glob("./blog/*.md", { eager: true })
+const posts = Object.values(postImportResults).filter(post => post.frontmatter.published)
+export function get() {
+
+	return rss({
+		stylesheet: "rss/styles.xsl",
+		title: 'Enjoygraphy',
+		description: 'A blossom in the dessert',
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: posts.map(post => ({
+			link: post.url,
+			pubDate: new Date(post.frontmatter.date).toLocaleString({ language: "en-IN" }),
+			title: post.frontmatter.title,
+			customData: `<author>Raj Patel</author>`,
+			description: post.frontmatter.description
+		}))
+
 	});
+}
