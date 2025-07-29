55e7688c0a241057e7002edae779eb3f8f0a2928
Raj Patel <rajkpatel1728@gmail.com>
2025-07-29 21:51:18 +0530

    Feat: modular navigation, header and footer updates

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,41 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'memory palace';
-export const SITE_DESCRIPTION = 'A site maintained by raj patel';
+export const SITE_TITLE = "Raj Patel"
+export const SITE_DESCRIPTION = "Writes about Programming, Philosophy and Movies";
+
+
+export const NAVIGATION = [
+    {
+        "text": "Home",
+        "link": "/"
+    },
+    // {
+    //     "text": "Books",
+    //     "link": "/books"
+    // },
+    {
+        "text": "Blog",
+        "link": "/blog"
+    },
+    {
+        "text": "Misc",
+        "link": "/misc"
+    },
+]
+
+import { Mail, Github, TwitterIcon } from "lucide-react";
+export const SOCIALS = [
+    {
+        "element": Mail,
+        "link": "mailto:rajpatel10953@gmail.com"
+    },
+    {
+        "element": Github,
+        "link": "https://github.com/imrraaj"
+    },
+    {
+        "element": TwitterIcon,
+        "link": "https://x.com/imrraaj"
+    },
+]
